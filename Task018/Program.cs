2723317684613a2b0c6a593e1ad3db128e5fc5e7
Julia Ch <juliecha@yandex.ru>
2022-10-07 20:39:39 +0300

    Add task018

---
+++ Task018/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Clear();
+Console.WriteLine("Введите номер четверти (от 1 до 4)");
+int x = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите Y");
+// int y = Convert.ToInt32(Console.ReadLine());
+
+if (x < 1 | x > 4)
+{
+    Console.WriteLine("Вы ввели некорректное значение");
+}
+else if (x == 1)
+{
+    Console.WriteLine("Первая четверть: x > 0 и y > 0");
+}
+else if (x == 2)
+{
+    Console.WriteLine("Вторая четверть: x < 0 и y > 0");
+}
+else if (x == 3)
+{
+    Console.WriteLine("Третья четверть: x < 0 и y < 0");
+}
+else if (x == 4)
+{
+    Console.WriteLine("Четвертая четверть: x > 0 и y < 0");
+}
