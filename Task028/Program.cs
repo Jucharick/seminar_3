183618159fc83cacbd155f393554f560da3bacf6
Julia Ch <juliecha@yandex.ru>
2022-10-11 21:24:14 +0300

    Add Task028

---
+++ Task028/Program.cs
@@ -0,0 +1,15 @@
+﻿// multiplication
+
+Console.WriteLine("Введите целое число n");
+int input = int.Parse(Console.ReadLine());
+
+int multiplication(int multip)
+{
+    multip = 1;
+    for (int i = 1; i <= input; i++)
+    {
+        multip = multip * i;
+    }
+    return multip;
+}
+Console.Write($"Произведение чисел от 1 до {input} = {multiplication(input)}");
