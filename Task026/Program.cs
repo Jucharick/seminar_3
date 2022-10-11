82e0f6cd6282c779550b80d3495d4c1501dea8fa
Julia Ch <juliecha@yandex.ru>
2022-10-11 20:36:18 +0300

    UPD Task026

---
+++ Task026/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+// Console.WriteLine("Введите целое число n");
+// string n = Console.ReadLine();
+// int length = n.Length;
+// Console.WriteLine(length);
+
+Console.WriteLine("Введите целое число n");
+int n = int.Parse(Console.ReadLine());
+int i = 0;
+while (n != 0)
+{
+    n = n / 10;
+    i++;
+}
+Console.WriteLine(i);
