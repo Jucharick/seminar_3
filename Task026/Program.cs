fa3aaac80eb77008fcd4520b9b37a62a3cb78bd8
Julia Ch <juliecha@yandex.ru>
2022-10-11 20:47:02 +0300

    UPD Task026

--- Task026/Program.cs
+++ Task026/Program.cs
@@ -3,21 +3,29 @@
 // 78 -> 2
 // 89126 -> 5
 
+
 // Console.WriteLine("Введите целое число n");
 // string n = Console.ReadLine();
 // int length = n.Length;
 // Console.WriteLine(length);
 
+
+// Console.WriteLine("Введите целое число n");
+// int n = int.Parse(Console.ReadLine());
+// int i = 0;
+// if (n == 0) Console.WriteLine(1);
+// else 
+// {
+//     while (n != 0)
+//     {
+//         n = n / 10;
+//         i++;
+//     }
+//     Console.WriteLine(i);
+// }
+
+
 Console.WriteLine("Введите целое число n");
 int n = int.Parse(Console.ReadLine());
-int i = 0;
-if (n == 0) Console.WriteLine(1);
-else 
-{
-    while (n != 0)
-    {
-        n = n / 10;
-        i++;
-    }
-    Console.WriteLine(i);
-}
+int lenght = (int)Math.Log10(n) + 1;
+Console.WriteLine(lenght);
