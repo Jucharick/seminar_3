3ce941673faa1e7c674c9dfa304801ac065eb4b6
Julia Ch <juliecha@yandex.ru>
2022-10-11 20:14:24 +0300

    Add Task024

---
+++ Task024/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите целое число n");
+int n = int.Parse(Console.ReadLine());
+int sum = 0;
+while (n < 1)
+{
+    Console.WriteLine("Значение некорректно, попробуйте еще раз: ");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+for (int i = 1; i <= n; i++)
+{
+    sum = sum + i;
+}
+Console.Write($"Сумма от 1 до N = {sum}");
