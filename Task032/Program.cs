80b2e53b340ca8d268a20a3e986466079abb6ad2
Julia Ch <juliecha@yandex.ru>
2022-10-14 22:23:22 +0300

    Add Task032

---
+++ Task032/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 32: Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2] 
+
+
+
+
+int[] array = new Int32[4];
+
+void FillArray(int[] array, int from, int to)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(from, to+1); 
+    }
+}
+ 
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}, "); 
+    }
+    Console.Write($"{array[array.Length - 1]}");
+    Console.WriteLine();
+}
+
+void signChange (int[] arr)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        arr[i] = arr[i] * -1;
+    }
+}
+
+FillArray(array, -9, 10);
+PrintArray(array);
+signChange(array);
+PrintArray(array);
