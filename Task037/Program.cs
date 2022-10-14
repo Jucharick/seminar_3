7945f3f66b8bfdac8de59b573adcf7bdec174935
Julia Ch <juliecha@yandex.ru>
2022-10-14 22:46:38 +0300

    UPD Task037

--- Task037/Program.cs
+++ Task037/Program.cs
@@ -31,10 +31,11 @@ int[] fillMass(int[] array, int from, int to)
 
 void printArray(int[] array)
 {
-    for (int i = 0; i < array.Length; i++)
+    for (int i = 0; i < array.Length - 1; i++)
     {
-        Console.Write(array[i] + " ");
+        Console.Write($"{array[i]}, ");
     }
+    Console.Write($"{array[array.Length - 1]}");
     Console.WriteLine();
 }
 
