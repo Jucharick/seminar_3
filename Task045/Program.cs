e4577e184d2eb2bdf956516cdb819041bbd42c50
Julia Ch <juliecha@yandex.ru>
2022-10-18 21:30:17 +0300

    Add Task045

---
+++ Task045/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 45: Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.
+
+
+int[] array1 = new Int32[10];
+int[] array2 = new Int32[10];
+
+void FillArray(int[] array, int from, int to)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(from, to);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.Write($"{array[array.Length - 1]}");
+    Console.WriteLine();
+}
+
+void CopyArray(int[] array1, int[] array2)
+{
+    for (int i = 0; i < array1.Length; i++)
+    {
+        array2[i] = array1[i];
+    }
+}
+
+
+
+FillArray(array1, 0, 100);
+PrintArray(array1);
+CopyArray(array1, array2);
+PrintArray(array2);
