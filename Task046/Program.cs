e85c907f1296e325eee2f451b9642585c50f4401
Julia Ch <juliecha@yandex.ru>
2022-10-21 20:14:52 +0300

    UPD Task046

--- Task046/Program.cs
+++ Task046/Program.cs
@@ -1,17 +1,5 @@
 ﻿int [,] matrix  = new int [3, 4];
 
-void PrintMatrix(int [,] matr) 
-{
-    for (int i = 0; i < matr.GetLength(0); i++) // идем по строкам
-    {
-        for (int j = 0; j < matr.GetLength(1); j++) // идем по колонкам
-        {
-        Console.Write($" {matr[i, j]} ");
-        }
-        Console.WriteLine();
-    }
-}
-
 void FillMatrix(int [,] matr)
 {
     for (int i = 0; i < matr.GetLength(0); i++) // идем по строкам
@@ -23,6 +11,17 @@ void FillMatrix(int [,] matr)
     }
 }
 
+void PrintMatrix(int [,] matr) 
+{
+    for (int i = 0; i < matr.GetLength(0); i++) // идем по строкам
+    {
+        for (int j = 0; j < matr.GetLength(1); j++) // идем по колонкам
+        {
+        Console.Write($" {matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
 
 FillMatrix(matrix);
 PrintMatrix(matrix);
