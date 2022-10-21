d00024c0a1f811ac2b0fd1163e7af0d924f7f1bc
Julia Ch <juliecha@yandex.ru>
2022-10-21 20:13:24 +0300

    Add Task046

---
+++ Task046/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+
+int [,] matrix  = new int [3, 4];
+
+void PrintMatrix(int [,] matr) 
+{
+    for (int i = 0; i < matr.GetLength(0); i++) // идем по строкам
+    {
+        for (int j = 0; j < matr.GetLength(1); j++) // идем по колонкам
+        {
+        Console.Write($" {matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillMatrix(int [,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++) // идем по строкам
+    {
+        for (int j = 0; j < matr.GetLength(1); j++) // идем по колонкам
+        {
+            matr [i, j] = new Random().Next(-10, 10); // интервал будте от -10 до 9 (10 не включается)
+        }
+    }
+}
+
+
+FillMatrix(matrix);
+PrintMatrix(matrix);
