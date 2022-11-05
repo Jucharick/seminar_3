6431e9e3db95177f6c2764376438a8950a9d2884
Julia Ch <juliecha@yandex.ru>
2022-11-05 20:51:44 +0300

    UPD Task039

--- Task039/Program.cs
+++ Task039/Program.cs
@@ -35,11 +35,25 @@ void PrintArray(int[] array)
 //     }
 // }
 
-
+void ReverseArray(int[] array) // имя метода должно отражать суть метода - реверсия массива
+{
+    int size = array.Length;
+    int index1 = 0;
+    int index2 = size - 1;
+    while (index1 < index2)
+    {
+        int temp = array[index1];
+        array[index1] = array[index2];
+        array[index2] = temp;
+        index1++;
+        index2--;
+    }
+}
 
 
 FillArray(array, -9, 10);
 PrintArray(array);
-Array.Reverse(array); // метод, что есть в c#
+// Array.Reverse(array); // встроенный метод, что есть в c#
 // Reverse(array);
+ReverseArray(array);
 PrintArray(array);
