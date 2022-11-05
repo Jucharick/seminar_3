93b0f20f38d25f2f4674d1e833704acb9e6a7faa
Julia Ch <juliecha@yandex.ru>
2022-11-05 23:56:58 +0300

    UPD Task048

--- Task048/Program.cs
+++ Task048/Program.cs
@@ -5,36 +5,72 @@
 // 2 3 4 5
 
 
-
 Console.WriteLine("Введите высоту матрицы (количество строк)");
-int m = int.Parse(Console.ReadLine());
+int rows = int.Parse(Console.ReadLine());
 Console.WriteLine("Введите длину матрицы (количество столбцов)");
-int n = int.Parse(Console.ReadLine());
+int columns = int.Parse(Console.ReadLine());
 
-int [,] matrix  = new int [m, n];
-
-void FillMatrix(int [,] matr)
-{
-    for (int i = 0; i < matr.GetLength(0); i++) // идем по строкам
-    {
-        for (int j = 0; j < matr.GetLength(1); j++) // идем по колонкам
-        {
-            matr [i, j] = i + j;
-        }
-    }
-}
-
-void PrintMatrix(int [,] matr) 
-{
-    for (int i = 0; i < matr.GetLength(0); i++) // идем по строкам
-    {
-        for (int j = 0; j < matr.GetLength(1); j++) // идем по колонкам
-        {
-        Console.Write($" {matr[i, j]} ");
-        }
-        Console.WriteLine();
-    }
-}
-
-FillMatrix(matrix);
+int [,] matrix  = CreateMatrixRndInt(rows, columns, -100, 100);
 PrintMatrix(matrix);
+
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    for (int i = 0; i < matrix.GetLength(0); i++) // 0 - rows
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) // 1 - columns
+        {
+            matrix[i, j] = i + j;
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int [,] matrix) 
+{
+    for (int i = 0; i < matrix.GetLength(0); i++) // идем по строкам
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++) // идем по колонкам
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($" {matrix[i, j], 4} | "); // 4 - для выравнивания вывода матрицы
+            else Console.Write($" {matrix[i, j], 4} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+
+// Console.WriteLine("Введите высоту матрицы (количество строк)");
+// int m = int.Parse(Console.ReadLine());
+// Console.WriteLine("Введите длину матрицы (количество столбцов)");
+// int n = int.Parse(Console.ReadLine());
+
+// int [,] matrix  = new int [m, n];
+
+// void FillMatrix(int [,] matr)
+// {
+//     for (int i = 0; i < matr.GetLength(0); i++) // идем по строкам
+//     {
+//         for (int j = 0; j < matr.GetLength(1); j++) // идем по колонкам
+//         {
+//             matr [i, j] = i + j;
+//         }
+//     }
+// }
+
+// void PrintMatrix(int [,] matr) 
+// {
+//     for (int i = 0; i < matr.GetLength(0); i++) // идем по строкам
+//     {
+//         for (int j = 0; j < matr.GetLength(1); j++) // идем по колонкам
+//         {
+//         Console.Write($" {matr[i, j]} ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// FillMatrix(matrix);
+// PrintMatrix(matrix);
--- Task049/Program.cs
+++ Task049/Program.cs
@@ -1,64 +1,114 @@
 ﻿// Задача 49: Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты.
+//
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+//
+//  1  4 49 2
+//  5  9  2 3
+// 64  4  4 4
 
 
 Console.WriteLine("Введите высоту матрицы (количество строк)");
-int m = int.Parse(Console.ReadLine());
+int rows = int.Parse(Console.ReadLine());
 Console.WriteLine("Введите длину матрицы (количество столбцов)");
-int n = int.Parse(Console.ReadLine());
+int columns = int.Parse(Console.ReadLine());
 
-int[,] matrix = new int[m, n];
+int [,] matrix  = CreateMatrixRndInt(rows, columns, 0, 10);
+PrintMatrix(matrix);
 
-void FillMatrix(int[,] matr)
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
 {
-    for (int i = 0; i < matr.GetLength(0); i++) // идем по строкам
+    int[,] matrix = new int[rows, columns];
+    var rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++) // 0 - rows
     {
-        for (int j = 0; j < matr.GetLength(1); j++) // идем по колонкам
+        for (int j = 0; j < matrix.GetLength(1); j++) // 1 - columns
         {
-            matr[i, j] = new Random().Next(1, 10); // интервал будте от -10 до 9 (10 не включается)
+            matrix[i, j] = rnd.Next(min, max + 1);
         }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int [,] matrix) 
+{
+    for (int i = 0; i < matrix.GetLength(0); i++) // идем по строкам
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++) // идем по колонкам
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($" {matrix[i, j], 4} | "); // 4 - для выравнивания вывода матрицы выводим по 4 символа (для того, чтобы при выводе числа с большей разрядностью матрица была ровной (100, 1000))
+            else Console.Write($" {matrix[i, j], 4} ");
+        }
+        Console.WriteLine("|");
     }
 }
 
-void PrintMatrix(int[,] matr)
-{
-    for (int i = 0; i < matr.GetLength(0); i++) // идем по строкам
-    {
-        for (int j = 0; j < matr.GetLength(1); j++) // идем по колонкам
-        {
-            Console.Write($" {matr[i, j]} ");
-        }
-        Console.WriteLine();
-    }
-}
 
-// void Squared(int[,] matr)
+
+
+
+
+// Console.WriteLine("Введите высоту матрицы (количество строк)");
+// int m = int.Parse(Console.ReadLine());
+// Console.WriteLine("Введите длину матрицы (количество столбцов)");
+// int n = int.Parse(Console.ReadLine());
+
+// int[,] matrix = new int[m, n];
+
+// void FillMatrix(int[,] matr)
 // {
 //     for (int i = 0; i < matr.GetLength(0); i++) // идем по строкам
 //     {
 //         for (int j = 0; j < matr.GetLength(1); j++) // идем по колонкам
 //         {
-//             if (i % 2 == 0 && j % 2 == 0)
-//             {
-//                 matr[i, j] = matr[i, j] * matr[i, j] ;
-//             }
+//             matr[i, j] = new Random().Next(1, 10); // интервал будте от -10 до 9 (10 не включается)
 //         }
 //     }
 // }
 
-void Squared(int[,] matr)
-{
-    for (int i = 0; i < matr.GetLength(0); i+=2) // идем по строкам
-    {
-        for (int j = 0; j < matr.GetLength(1); j+=2) // идем по колонкам
-        {
-                matr[i, j] = matr[i, j] * matr[i, j] ; // так алгоритм работает быстрее, чем с if
-        }
-    }
-}
+// void PrintMatrix(int[,] matr)
+// {
+//     for (int i = 0; i < matr.GetLength(0); i++) // идем по строкам
+//     {
+//         for (int j = 0; j < matr.GetLength(1); j++) // идем по колонкам
+//         {
+//             Console.Write($" {matr[i, j]} ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// // void Squared(int[,] matr)
+// // {
+// //     for (int i = 0; i < matr.GetLength(0); i++) // идем по строкам
+// //     {
+// //         for (int j = 0; j < matr.GetLength(1); j++) // идем по колонкам
+// //         {
+// //             if (i % 2 == 0 && j % 2 == 0)
+// //             {
+// //                 matr[i, j] = matr[i, j] * matr[i, j] ;
+// //             }
+// //         }
+// //     }
+// // }
+
+// void Squared(int[,] matr)
+// {
+//     for (int i = 0; i < matr.GetLength(0); i+=2) // идем по строкам
+//     {
+//         for (int j = 0; j < matr.GetLength(1); j+=2) // идем по колонкам
+//         {
+//                 matr[i, j] = matr[i, j] * matr[i, j] ; // так алгоритм работает быстрее, чем с if
+//         }
+//     }
+// }
 
 
-FillMatrix(matrix);
-PrintMatrix(matrix);
-Console.WriteLine();
-Squared(matrix);
-PrintMatrix(matrix);
+// FillMatrix(matrix);
+// PrintMatrix(matrix);
+// Console.WriteLine();
+// Squared(matrix);
+// PrintMatrix(matrix);
