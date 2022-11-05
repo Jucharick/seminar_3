30c815c975ca3506c46fd49248f9d9bbcdc05e0d
Julia Ch <juliecha@yandex.ru>
2022-11-05 23:32:28 +0300

    UPD Create matrix - Task046

--- Task046/Program.cs
+++ Task046/Program.cs
@@ -1,32 +1,45 @@
 ﻿Console.WriteLine("Введите высоту матрицы (количество строк)");
-int m = int.Parse(Console.ReadLine());
+int rows = int.Parse(Console.ReadLine());
 Console.WriteLine("Введите длину матрицы (количество столбцов)");
-int n = int.Parse(Console.ReadLine());
+int columns = int.Parse(Console.ReadLine());
 
-int [,] matrix  = new int [m, n];
+int [,] matrix  = CreateMatrixRndInt(rows, columns, 0, 10);
+PrintMatrix(matrix);
 
-void FillMatrix(int [,] matr)
+
+// void FillMatrix(int [,] matr)
+// {
+//     for (int i = 0; i < matr.GetLength(0); i++) // идем по строкам
+//     {
+//         for (int j = 0; j < matr.GetLength(1); j++) // идем по колонкам
+//         {
+//             matr [i, j] = new Random().Next(-10, 10); // интервал будте от -10 до 9 (10 не включается)
+//         }
+//     }
+// }
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
 {
-    for (int i = 0; i < matr.GetLength(0); i++) // идем по строкам
+    int[,] matrix = new int[rows, columns];
+    var rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++) // 0 - rows
     {
-        for (int j = 0; j < matr.GetLength(1); j++) // идем по колонкам
+        for (int j = 0; j < matrix.GetLength(1); j++) // 1 - columns
         {
-            matr [i, j] = new Random().Next(-10, 10); // интервал будте от -10 до 9 (10 не включается)
+            matrix[i, j] = rnd.Next(min, max + 1);
         }
     }
+    return matrix;
 }
 
-void PrintMatrix(int [,] matr) 
+void PrintMatrix(int [,] matrix) 
 {
-    for (int i = 0; i < matr.GetLength(0); i++) // идем по строкам
+    for (int i = 0; i < matrix.GetLength(0); i++) // идем по строкам
     {
-        for (int j = 0; j < matr.GetLength(1); j++) // идем по колонкам
+        for (int j = 0; j < matrix.GetLength(1); j++) // идем по колонкам
         {
-        Console.Write($" {matr[i, j]} ");
+        Console.Write($" {matrix[i, j]} ");
         }
         Console.WriteLine();
     }
 }
-
-FillMatrix(matrix);
-PrintMatrix(matrix);
