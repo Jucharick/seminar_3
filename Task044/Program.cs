dadd8c473b21e50a8dbbfeb09afe1b6c23d901a7
Julia Ch <juliecha@yandex.ru>
2022-11-05 22:26:53 +0300

    Add void Fibonacci at Task044

--- Task044/Program.cs
+++ Task044/Program.cs
@@ -8,17 +8,34 @@
 // f(n) = f(n-1) + f(n-2)
 
 Console.WriteLine("Введите количество:");
-int a = int.Parse(Console.ReadLine());
+int number = int.Parse(Console.ReadLine());
 
 Console.Write("0 1");
 int first = 0;
 int second = 1;
 int next = 0;
 
-for (int i = 1; i < a - 1; i++) // выведите первые N чисел
+for (int i = 1; i < number - 1; i++) // выведите первые N чисел
 {
     next = first + second;
     first = second;
     second = next;
     Console.Write(" " + next);
 }
+
+void Fibonacci (int num)
+{
+    int num1 = 0;
+    int num2 = 1;
+    int next = default;
+    Console.Write($"{num1} {num2}");
+    for (int i = 2; i < num; i++) // начинаем вывод с индекса 2 (с третьего числа), т.к. первые два числа просто выведем 
+    {
+        next = num1 + num2;
+        Console.Write($" {next}");
+        num1 = num2;
+        num2 = next;
+    }
+}
+Console.WriteLine();
+Fibonacci(number);
