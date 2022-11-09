47c177825d7d1f8c5f33f2c6a73adce5395bcb00
Julia Ch <juliecha@yandex.ru>
2022-11-09 19:24:51 +0300

    UPD recursion Pyramid game

--- recursion_Pyramid_game/Program.cs
+++ recursion_Pyramid_game/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Игра в пирамидки
+
+
+void Towers(string with = "1", string on = "3", string some = "2", int count = 4)
+{
+    if (count > 1) Towers(with, some, on, count - 1);
+    Console.WriteLine($"{with} >> {on}");
+    if (count > 1) Towers(some, on, with, count - 1);
+}
+
+Towers();
