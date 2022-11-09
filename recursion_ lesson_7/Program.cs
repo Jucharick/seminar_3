d0b1fd622d4875cf798f5695062411ecf5f24e8f
Julia Ch <juliecha@yandex.ru>
2022-11-09 18:48:40 +0300

    working with directories

--- recursion_ lesson_7/Program.cs
+++ recursion_ lesson_7/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Работа с директориями
+
+
+string path = "/Users/Юлия Чарикова/Desktop/GeekBrains/I четверть/3_Знакомство с языками программирования/Урок 1. ДЗ/Ex057";
+DirectoryInfo di = new DirectoryInfo(path);
+Console.WriteLine(di.CreationTime); // показывает когда была создана папка
+FileInfo[] fi = di.GetFiles(); // показывает что хранится в папке => создает массив
+
+for (int i = 0; i < fi.Length; i++)
+{
+    Console.WriteLine(fi[i].Name); // показывает имя файлов
+}
