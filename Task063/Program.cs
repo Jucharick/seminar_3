ae958cfb3839707be01ec296ce71bcb08e7e7795
Julia Ch <juliecha@yandex.ru>
2022-11-11 20:47:00 +0300

    UPD Task063

--- Task063/Program.cs
+++ Task063/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 63: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+// Задача 65: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 6, 7, 8"
+
+
+
+Console.WriteLine("Задайте значение N");
+int n = int.Parse(Console.ReadLine());
+
+PrintNaturalNumbers(n);
+
+void PrintNaturalNumbers(int n)
+{
+    int number = 1;
+    if (number <= n) 
+    {
+        PrintNaturalNumbers(n-1);
+        Console.Write($" {n}");
+    }
+}
